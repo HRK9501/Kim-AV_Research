2aa6a9389fb0fa968cc94cb4fdc4a74958193f6e
hrk9501 <h_kim90825@fanshaweonline.ca>
2020-04-03 15:21:34 -0400

    build body layout

---
+++ admin/admin_get_movies.php
@@ -0,0 +1,8 @@
+<?php
+    require_once '../load.php';
+
+    if (isset($_GET['allmovies'])) {
+        $movies = getAllMovies();
+
+        echo $movies;
+    }
--- js/components/TheTvComponent.js
+++ js/components/TheTvComponent.js
@@ -3,17 +3,17 @@ export default {
     
     template: `
     <div class="row">
-        <div class="col-12 order-2 order-md-1 col-md-3 media-container">
-            <h4 class="media-title">Tv Title Here</h4>
-            <p class="media-details">Storyline here</p>
-            <span class="media-time">runtime here</span>
-            <span class="media-year">Released in </span>
-        </div>
-
         <div class="col-12 order-1 order-md-2 col-md-9 media-container">
-        <video autoplay controls muted src="video/trailer.mp4" class="fs-video"></video>
+            <video autoplay controls muted src="video/friendsopening.mp4" class="fs-video"></video>
+        </div>    
+
+        <div class="col-12 order-2 order-md-1 col-md-3 media-container">
+            <h4 class="media-title">Friends</h4>
+            <p class="media-details">Friends is an American sitcom television series, created by David Crane and Marta Kauffman, which aired on NBC from September 22, 1994, to May 6, 2004, lasting ten seasons.</p>
+            <span class="media-time">20m</span>
+            <span class="media-year">1994</span>
+        </div>
     </div>
-</div>
 `,
 
 }
