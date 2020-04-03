026bdc287e25f97496b04eb32b115847ff2690a9
hrk9501 <h_kim90825@fanshaweonline.ca>
2020-04-03 13:52:29 -0400

    upload layout

---
+++ js/main.js
@@ -0,0 +1,27 @@
+import AudioComponent from "./components/TheAudioComponent.js";
+import MovieComponent from "./components/TheMovieComponent.js";
+import TvComponent from "./components/TheTvComponent.js";
+
+(() => {
+    const myVM = new Vue({
+        data: {
+            // set the default component! - you can find this in index.html
+            activeComponent: MovieComponent
+        },
+
+        methods: {
+            switchMovie() {
+                this.activeComponent = MovieComponent
+            },
+
+            switchAudio() {
+                 this.activeComponent = AudioComponent
+            },
+
+            switchTv() {
+                this.activeComponent = TvComponent
+            }
+
+        }
+    }).$mount("#app");
+})();
